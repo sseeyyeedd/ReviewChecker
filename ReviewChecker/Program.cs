2e6f6eaf62d4969a52e1143e7a9b111de666f0f2
sseeyyeedd <sseeyyeeddaalliisah@gmail.com>
2024-03-22 13:55:11 +0330

    Add project files.

---
+++ ReviewChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReviewChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the review:");
+                string review = Console.ReadLine();
+                var sampleData = new SentimentModel.ModelInput()
+                {
+                    Col0 = review
+                };
+                var result = SentimentModel.Predict(sampleData);
+                var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+                Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+            }
+            
+        }
+    }
+}
